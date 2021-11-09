82690447f6b9b34c3df90dc9d381a259a8e52fc2
AmritcsAdhikari <amritcsadhikari@gmail.com>
2021-11-09 13:22:00 -0600

    add httpSessionDemo

---
+++ com.test.httpSessionDemo/src/main/java/com/test/httpSessionDemo/com/test/httpSessionDemo/ServletTwo.java
@@ -0,0 +1,37 @@
+package com.test.httpSessionDemo.com.test.httpSessionDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ServletTwo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		HttpSession session = request.getSession(false);
+		
+		String n = (String) session.getAttribute("name");
+		
+		out.print("Hello " + n);
+		out.close();
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ com.test.httpSessionDemo/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>HTTP Session Demo</title>
+</head>
+<body>
+	<form action="ServletOne">
+	Name: <input type="text" name="name" placeholder="Name">
+	<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
